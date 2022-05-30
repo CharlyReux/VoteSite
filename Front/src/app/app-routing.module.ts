1823492a0aa943d92ed8e6047f6d8f05d24e3c9c
charly <charlyreux@gmail.com>
2022-05-30 10:34:05 +0200

    file rename

---
+++ Front/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+
+const routes: Routes = [
+  {path:'',component:WelcomePageComponent}
+  ,
+  { 
+    path: '**', 
+    redirectTo: ''
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
