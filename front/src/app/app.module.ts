0017b8ebbc47b8b621c5489eec358a5fcd802e79
charly <charlyreux@gmail.com>
2022-05-31 12:04:22 +0200

    front page

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { MatButtonModule } from '@angular/material/button'
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatIconModule
   ],
-  providers: [],
+  providers: [WelcomePageComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
